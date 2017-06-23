d7ac209af911a5109f07f9c452b5171601a2d7f5
REKSOFT\chaplygin <chaplygin@reksoft.ru>
2017-06-23 17:26:42 +0300

    сервлет для Sling Models + template и новая страница

---
+++ aemproject/bundle/src/main/java/com/aem/community/SlingModelsServlet.java
@@ -0,0 +1,56 @@
+package com.aem.community;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import com.aem.models.TrainingPage;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by chaplygin on 23.06.2017.
+ */
+
+@SlingServlet(paths="/bin/slingmodel", methods="GET")
+public class SlingModelsServlet extends SlingAllMethodsServlet{
+
+    private static final long serialVersionUID = 1L;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Reference
+    ResourceResolverFactory resourceResolverFactory;
+    ResourceResolver resourceResolver;
+    public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)throws ServletException,IOException{
+        logger.info("inside sling model test servlet");
+        response.setContentType("text/html");
+        try {
+            resourceResolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+            Resource resource = resourceResolver.getResource("/content/kdad/training-general-page/page-stage/jcr:content");
+            ValueMap valueMap=resource.adaptTo(ValueMap.class);
+
+            response.getWriter().write("Output from ValueMap is template: "+valueMap.get("cq:template").toString()+"");
+
+            TrainingPage trainingPage = resource.adaptTo(TrainingPage.class);
+            response.getWriter().write("Output from Sling Model is title: "+trainingPage.getTitle()+" Resource: "+trainingPage.getResource()+" Path: "+trainingPage.getResource().getPath());
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        finally{
+            if(resourceResolver.isLive())
+                resourceResolver.close();
+        }
+
+    }
+
+}
---
+++ aemproject/content/src/main/content/jcr_root/apps/kdad/components/page/templateModel/templateModel.jsp
@@ -0,0 +1,48 @@
+<%@include file="/libs/foundation/global.jsp" %>
+<cq:includeClientLib categories="aem.training"/>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+
+        $(document).ready(function () {
+
+            $('#submit').click(function () {
+                var failure = function (err) {
+                    alert("Unable to retrive data " + err);
+                };
+
+                $.ajax({
+                    type: 'GET',
+                    url: '/bin/slingmodel',
+                    success: function (msg) {
+                        $('#json').val(msg);
+                    }
+                });
+            });
+
+        });
+    </script>
+</head>
+
+<title>Sling Model Page</title>
+
+<body>
+
+<h1>Sling Model </h1>
+
+<form method="#">
+
+<textarea id="json" rows="15" cols="200">
+</textarea>
+
+    <input type="button" value="Get Sling Model Data" name="submit" id="submit">
+
+</form>
+
+</body>
+
+</html>
